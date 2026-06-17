99d6a17f606929afc2d256fc32a1257ea1d90bf3
exxecc <dosequis.aj@gmail.com>
2026-06-17 01:30:23 -0700

    Add files via upload

---
+++ index.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import MeetDayCalc from "./meet-day-calculator";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <MeetDayCalc />
+  </React.StrictMode>
+);
